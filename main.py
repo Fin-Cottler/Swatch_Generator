95daa38f536d8335adbab105202580ebffceb803
Fin-Cottler <fin.cottler@gmail.com>
2026-02-02 09:39:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from colorSwatch import *
 '''
-populate_swatches generates png files (25px x 25px) of maximum saturation colors between two hex code colors
+populate_swatches generates png files (25px x 25px) of maximum saturation and value colors between two hex code colors
 populate_swatches takes 4 parameters:
+
     hex_code (str): this is one of the two color end points. It must be a hexcode without its prefix '#'
+    
     other_hex_code (str): this is the second of the two color end points. It must be a hexcode without its prefix '#'
+    
     colors_count (int): this is the number of swatches that should be generated
+    
     directory (str NOTE: must be raw string on windows by prefixing string with 'r'): this is the full path to folder where swatches will be placed
         IMPORTANT NOTE: DIRECTORY MUST ALREADY EXIST (and preferably empty) !!!
 '''
 
+'''
+Reasoning for maximum saturation and value:
+
+    No matter the saturation of the two input colors, the saturation and value will be increased to their maximum, only preserving their hue data.
+    This isolates hue as a single parameter for evaluation
+'''
 
 Colorswatch.populate_swatches(
     "3270a6",
@@ -16,4 +26,5 @@ Colorswatch.populate_swatches(
     20,
     r"C:\Users\gelat\Downloads\Swatch_Generator-main\Swatch_Generator-main\swatches"
 )
+
 
